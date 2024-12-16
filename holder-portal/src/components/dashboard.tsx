4b358257c6a71b56b430e3f7c40edadebd59d151
bhavanakarwade <bhavana.karwade@ayanworks.com>
2024-12-16 15:16:45 +0530

    fix: added pagination logic
    Signed-off-by: bhavanakarwade <bhavana.karwade@ayanworks.com>

--- holder-portal/src/components/dashboard.tsx
+++ holder-portal/src/components/dashboard.tsx
@@ -21,6 +21,8 @@ type Token = {
 const Dashboard = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [tokensList, setTokensList] = useState<Token[]>([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 10;
 
   const Headers = [
     "Token Id",
@@ -30,53 +32,24 @@ const Dashboard = () => {
     "NFTsList",
     "Action",
   ];
-  // // Placeholder data
-  // const holderAccount = {
-  //   accountId: "0.0.123456",
-  //   balance: 10000, // in HBAR
-  //   tokens: [
-  //     {
-  //       name: "Token One",
-  //       symbol: "TOK1",
-  //       id: "0.0.111",
-  //       balance: 250,
-  //       logo: "https://via.placeholder.com/48",
-  //     },
-  //     {
-  //       name: "Token Two",
-  //       symbol: "TOK2",
-  //       id: "0.0.112",
-  //       balance: 400,
-  //       logo: "https://via.placeholder.com/48",
-  //     },
-  //     {
-  //       name: "Token Three",
-  //       symbol: "TOK3",
-  //       id: "0.0.113",
-  //       balance: 123,
-  //       logo: "https://via.placeholder.com/48",
-  //     },
-  //   ],
-  // };
 
   useEffect(() => {
     const getAccountId: string | null = localStorage.getItem("accountId");
-    handleCreateToken(getAccountId);
+    fetchTokens(getAccountId);
   }, []);
-  const handleCreateToken = async (accountId: string | null) => {
+
+  const fetchTokens = async (accountId: string | null) => {
     setIsLoading(true);
     try {
       const body = {
         "account.id": accountId,
-        limit: 2,
         order: "desc",
         type: "NON_FUNGIBLE_UNIQUE",
       };
 
-      const response = await axios.get(
-        `${envConfig.PUBLIC_BASE_URL}${apiRoutes.tokens}`,
-        { params: body }
-      );
+      const url = `${envConfig.PUBLIC_BASE_URL}${apiRoutes.tokens}`;
+
+      const response = await axios.get(url, { params: body });
 
       if (!response.data) {
         throw new Error("Error creating token");
@@ -91,14 +64,26 @@ const Dashboard = () => {
   };
 
   const NavigationToScreen = (route: string, token_id: string) => {
-    const tokenData = {
-      tokenId: token_id,
-    };
-    // Set the tokenData
+    const tokenData = { tokenId: token_id };
     localStorage.setItem("tokenData", JSON.stringify(tokenData));
-
-    // Navigate to the next page
     window.location.href = route;
+  };
+
+  const totalPages = Math.ceil(tokensList.length / itemsPerPage);
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+  const currentTokens = tokensList.slice(startIndex, endIndex);
+
+  const handleNextPage = () => {
+    if (currentPage < totalPages) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+
+  const handlePreviousPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
   };
 
   return (
@@ -109,160 +94,84 @@ const Dashboard = () => {
         <Navbar />
 
         <main className="flex-grow p-6">
-          {/* <h2 className="text-2xl font-semibold text-blue-800 mb-6">
-            Dashboard
-          </h2> */}
-
-          {/* Tabs */}
-          {/* <div className="mb-4 border-b border-gray-200 dark:border-gray-700">
-            <ul
-              className="pl-5 flex flex-wrap -mb-px text-sm font-medium text-center"
-              role="tablist"
-            >
-              <li className="mr-2">
-                <button
-                  className={`text-xl inline-block p-4 border-b-2 rounded-t-lg ${
-                    activeTab === "balance"
-                      ? "text-blue-600 border-blue-600"
-                      : "text-gray-500 hover:text-gray-600 hover:border-gray-300"
-                  }`}
-                  onClick={() => setActiveTab("balance")}
-                  type="button"
-                  role="tab"
-                  aria-selected={activeTab === "balance"}
-                >
-                  Balance
-                </button>
-              </li>
-              <li className="mr-2">
-                <button
-                  className={`text-xl inline-block p-4 border-b-2 rounded-t-lg ${
-                    activeTab === "tokens"
-                      ? "text-blue-600 border-blue-600"
-                      : "text-gray-500 hover:text-gray-600 hover:border-gray-300"
-                  }`}
-                  onClick={() => setActiveTab("tokens")}
-                  type="button"
-                  role="tab"
-                  aria-selected={activeTab === "tokens"}
-                >
-                  Token Details
-                </button>
-              </li>
-            </ul>
-          </div> */}
-
-          {/* Tab Content */}
-          {/* <div className="my-12">
-            <h4 className="text-blue-800 text-3xl font-bold">
-              Digital Security Details
-            </h4>
-          </div> */}
-          <div>
-            {/* {activeTab === "balance" && (
-              <div className="relative w-full max-w-lg mx-auto mt-6">
-              
-                <div className="bg-gradient-to-tr from-blue-600 to-cyan-400 text-white p-8 rounded-lg shadow-lg flex flex-col items-center space-y-6">
-                  <h3 className="text-3xl font-bold text-center">
-                    Current Available Balance
-                  </h3>
-
-                  <div className="flex flex-col items-center space-y-4">
-                    <p className="text-lg font-semibold">
-                      <span className="text-xl">Account ID:</span>{" "}
-                      {holderAccount.accountId}
-                    </p>
-
-                    <hr className="border-white my-4 w-full" />
-
-                   
-                    <div className="flex flex-col items-center">
-                      <div className="text-6xl font-bold">
-                        100
-                        <span className="text-sm font-normal"> ATS</span>
-                      </div>
-
-                      <p className="text-lg font-semibold mt-4">
-                        <span className="text-xl">Value:</span> $1,00,00
+          <div className="relative flex flex-col w-full h-full overflow-scroll text-gray-700 bg-white shadow-md rounded-lg bg-clip-border">
+          <h2 className="text-2xl font-semibold text-blue-800 mb-6 ml-4 ">
+            TokenList
+            </h2>
+            <table className="w-full text-left table-auto min-w-max">
+              <thead>
+                <tr>
+                  {Headers.map((header, index) => (
+                    <th key={index} className="p-4 pt-7 pb-7 border-b border-slate-300 bg-slate-50">
+                      <p className="block text-sm font-bold leading-none text-slate-500">
+                        {header}
                       </p>
-                    </div>
-                  </div>
-                </div>
+                    </th>
+                  ))}
+                </tr>
+              </thead>
+              <tbody>
+                {currentTokens.map((token, index) => (
+                  <tr key={index} className="hover:bg-slate-50">
+                    <td className="p-4 border-b border-slate-200">
+                      <p className="block text-sm text-slate-800">{token.token_id}</p>
+                    </td>
+                    <td className="p-4 border-b border-slate-200">
+                      <p className="block text-sm text-slate-800">{token.name}</p>
+                    </td>
+                    <td className="p-4 border-b border-slate-200">
+                      <p className="block text-sm text-slate-800">{token.symbol}</p>
+                    </td>
+                    <td className="p-4 border-b border-slate-200">
+                      <p className="block text-sm text-slate-800">{token.type}</p>
+                    </td>
+                    <td className="p-4 border-b border-slate-200">
+                      <button
+                        onClick={() => NavigationToScreen("/nftsList", token.token_id)}
+                        type="button"
+                        className="rounded border-2 border-blue-500 px-2 py-1 text-xs font-medium text-blue-600 transition duration-150 hover:border-blue-600 hover:bg-blue-50 hover:text-blue-700"
+                      >
+                        NFTs
+                      </button>
+                    </td>
+                    <td className="p-4 border-b border-slate-200">
+                      <button
+                        onClick={() => NavigationToScreen("/tokenDetail", token.token_id)}
+                        type="button"
+                        className="rounded border-2 border-blue-500 px-2 py-1 text-xs font-medium uppercase text-blue-600 transition duration-150 hover:border-blue-600 hover:bg-blue-50 hover:text-blue-700"
+                      >
+                        View
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
 
-               
+            {/* Pagination Controls */}
+            {tokensList.length > itemsPerPage && (
+              <div className="flex justify-between items-center p-4">
                 <button
-                  className="absolute bottom-6 right-6 px-6 py-3 bg-green-500 text-white font-semibold rounded-lg hover:bg-green-400 transition"
-                  
+                  onClick={handlePreviousPage}
+                  disabled={currentPage === 1}
+                  className={`px-4 py-2 border rounded ${currentPage === 1 ? 'bg-gray-200 text-gray-400' : 'bg-blue-500 text-white'}`}
                 >
-                  Transfer
+                  Previous
+                </button>
+
+                <p className="text-sm text-gray-700">
+                  Page {currentPage} of {totalPages}
+                </p>
+
+                <button
+                  onClick={handleNextPage}
+                  disabled={currentPage === totalPages}
+                  className={`px-4 py-2 border rounded ${currentPage === totalPages ? 'bg-gray-200 text-gray-400' : 'bg-blue-500 text-white'}`}
+                >
+                  Next
                 </button>
               </div>
-            )} */}
-
-            <div className="relative flex flex-col w-full h-full overflow-scroll text-gray-700 bg-white shadow-md rounded-lg bg-clip-border">
-              <table className="w-full text-left table-auto min-w-max">
-                <thead>
-                  <tr>
-                    {Headers.map((header) => (
-                      <th className="p-4 pt-7 pb-7 border-b border-slate-300 bg-slate-50">
-                        <p className="block text-sm font-bold leading-none text-slate-500">
-                          {header}
-                        </p>
-                      </th>
-                    ))}
-                  </tr>
-                </thead>
-                <tbody>
-                  {/* Render the Token List */}
-                  {tokensList?.map((token, index) => (
-                    <tr key={index} className="hover:bg-slate-50">
-                      <td className="p-4 border-b border-slate-200">
-                        <p className="block text-sm text-slate-800">
-                          {token.token_id}
-                        </p>
-                      </td>
-                      <td className="p-4 border-b border-slate-200">
-                        <p className="block text-sm text-slate-800">
-                          {token.name}
-                        </p>
-                      </td>
-                      <td className="p-4 border-b border-slate-200">
-                        <p className="block text-sm text-slate-800">
-                          {token.symbol}
-                        </p>
-                      </td>
-                      <td className="p-4 border-b border-slate-200">
-                        <p className="block text-sm text-slate-800">
-                          {token.type}
-                        </p>
-                      </td>
-                      <td className="p-4 border-b border-slate-200">
-                        <button
-                          onClick={() => {
-                            NavigationToScreen("/nftsList", token.token_id);
-                          }}
-                          type="button"
-                          className="rounded border-2 border-blue-500 px-2 py-1 text-xs font-medium text-blue-600 transition duration-150 hover:border-blue-600 hover:bg-blue-50 hover:text-blue-700"
-                        >
-                          NFTs
-                        </button>
-                      </td>
-                      <td className="p-4 border-b border-slate-200">
-                        <button
-                          onClick={() => {
-                            NavigationToScreen("/tokenDetail", token.token_id);
-                          }}
-                          type="button"
-                          className="rounded border-2 border-blue-500 px-2 py-1 text-xs font-medium uppercase text-blue-600 transition duration-150 hover:border-blue-600 hover:bg-blue-50 hover:text-blue-700"
-                        >
-                          View
-                        </button>
-                      </td>
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
-            </div>
+            )}
           </div>
         </main>
       </div>
@@ -271,3 +180,4 @@ const Dashboard = () => {
 };
 
 export default Dashboard;
+
--- holder-portal/src/components/nftsList.tsx
+++ holder-portal/src/components/nftsList.tsx
@@ -7,38 +7,43 @@ import Sidebar from "./sidebar";
 
 type Token = {
   admin_key: {
-    _type: string; // Represents the type of key, e.g., "ECDSA_SECP256K1"
-    key: string; // The actual key value
+    _type: string; 
+    key: string; 
   };
-  metadata: string; // Metadata as a string
-  name: string; // Name of the token
-  symbol: string; // Symbol of the token
-  token_id: string; // Unique token identifier
-  type: "NON_FUNGIBLE_UNIQUE" | string; // Token type, default is "NON_FUNGIBLE_UNIQUE"
-  decimals: number; // Decimals (usually 0 for non-fungible tokens)
+  metadata: string; 
+  name: string; 
+  symbol: string; 
+  token_id: string; 
+  type: "NON_FUNGIBLE_UNIQUE" | string; 
+  decimals: number; 
+};
+
+type NFT = {
+  account_id: string,
+  created_timestamp: string,
+  delegating_spender: null,
+  deleted: boolean,
+  metadata: string,
+  serial_number: number,
+  spender: null,
+  token_id: string
 };
 
 const NftsList = () => {
   const [activeTab, setActiveTab] = useState("balance");
   const [isLoading, setIsLoading] = useState(false);
-
   const [nftsList, setNftsList] = useState([]);
-  const Headers = [
-    "Token Id",
-    "Token Name",
-    "Token Symbol",
-    "Token Type",
-    "Action",
-  ];
+  const Headers = ["Token Id", "serial No", "Created at", "Account Id"];
 
   useEffect(() => {
     getNfts();
   }, []);
+
   const getNfts = async () => {
     const getFromLocal = localStorage.getItem("tokenData");
     const parsedData = getFromLocal ? JSON.parse(getFromLocal) : null;
+    const token_id = parsedData?.tokenId; 
 
-    const token_id = parsedData.tokenId; // Access the token_id field
     setIsLoading(true);
     try {
       const response = await axios.get(
@@ -60,18 +65,19 @@ const NftsList = () => {
   return (
     <div className="flex bg-blue-200 min-h-screen">
       <Sidebar />
-
       <div className="flex-1 flex flex-col">
         <Navbar />
-
         <main className="flex-grow p-6">
           <div>
             <div className="relative flex flex-col w-full h-full overflow-scroll text-gray-700 bg-white shadow-md rounded-lg bg-clip-border">
               <table className="w-full text-left table-auto min-w-max">
                 <thead>
                   <tr>
-                    {Headers.map((header) => (
-                      <th className="p-4 pt-7 pb-7 border-b border-slate-300 bg-slate-50">
+                    {Headers.map((header, index) => (
+                      <th 
+                        key={index} 
+                        className="p-4 pt-7 pb-7 border-b border-slate-300 bg-slate-50"
+                      >
                         <p className="block text-sm font-bold leading-none text-slate-500">
                           {header}
                         </p>
@@ -80,50 +86,44 @@ const NftsList = () => {
                   </tr>
                 </thead>
                 <tbody>
-                  {/* Render the Token List */}
-                  {nftsList?.map((token, index) => (
-                    <tr key={index} className="hover:bg-slate-50">
-                      <td className="p-4 border-b border-slate-200">
-                        <p className="block text-sm text-slate-800">
-                          {"token.token_id"}
-                        </p>
-                      </td>
-                      <td className="p-4 border-b border-slate-200">
-                        <p className="block text-sm text-slate-800">
-                          {"token.name"}
-                        </p>
-                      </td>
-                      <td className="p-4 border-b border-slate-200">
-                        <p className="block text-sm text-slate-800">
-                          {"token.symbol"}
-                        </p>
-                      </td>
-                      <td className="p-4 border-b border-slate-200">
-                        <p className="block text-sm text-slate-800">
-                          {"token.type"}
-                        </p>
-                      </td>
-
-                      <td className="p-4 border-b border-slate-200">
-                        <button
-                          onClick={() => {
-                            // Set the tokenData
-                            // const tokenData = {
-                            //   tokenId: token.token_id,
-                            // };
-                            // localStorage.setItem(
-                            //   "tokenData",
-                            //   JSON.stringify(tokenData)
-                            // );
-                          }}
-                          type="button"
-                          className="rounded border-2 border-blue-500 px-2 py-1 text-xs font-medium uppercase text-blue-600 transition duration-150 hover:border-blue-600 hover:bg-blue-50 hover:text-blue-700"
-                        >
-                          View
-                        </button>
+                  {isLoading ? (
+                    <tr>
+                      <td colSpan={Headers.length} className="p-4 text-center">
+                        Loading...
                       </td>
                     </tr>
-                  ))}
+                  ) : nftsList.length > 0 ? (
+                    nftsList.map((token: NFT, index) => (
+                      <tr key={index} className="hover:bg-slate-50">
+                        <td className="p-4 border-b border-slate-200">
+                          <p className="block text-sm text-slate-800">
+                            {token.token_id}
+                          </p>
+                        </td>
+                        <td className="p-4 border-b border-slate-200">
+                          <p className="block text-sm text-slate-800">
+                            {token.serial_number}
+                          </p>
+                        </td>
+                        <td className="p-4 border-b border-slate-200">
+                          <p className="block text-sm text-slate-800">
+                          {new Date(Number(token.created_timestamp) * 1000).toLocaleString()}                          
+                          </p>
+                        </td>
+                        <td className="p-4 border-b border-slate-200">
+                          <p className="block text-sm text-slate-800">
+                            {token.account_id}
+                          </p>
+                        </td>
+                      </tr>
+                    ))
+                  ) : (
+                    <tr>
+                      <td colSpan={Headers.length} className="p-4 text-center text-slate-500">
+                        No NFTs found against this account
+                      </td>
+                    </tr>
+                  )}
                 </tbody>
               </table>
             </div>
